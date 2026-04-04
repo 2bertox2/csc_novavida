1706885e28e27a38a6855d3c04d8bbae691ae25a
Humberto Xavier <2bertox2@gmail.com>
2026-04-03 22:29:59 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,14 +1,19 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// O Firebase precisa dessa inicialização vazia no Service Worker
+// A configuração precisa estar completa também no arquivo de segundo plano
 firebase.initializeApp({
-    projectId: "csc-live-bh" // O nome do projeto que você criou
+  apiKey: "AIzaSyC63gBQ7JdRTnNCpSpir1brYH754fcKq9M",
+  authDomain: "csc-live-bh.firebaseapp.com",
+  projectId: "csc-live-bh",
+  storageBucket: "csc-live-bh.firebasestorage.app",
+  messagingSenderId: "345272283586",
+  appId: "1:345272283586:web:648c1fb52622792406665b"
 });
 
 const messaging = firebase.messaging();
 
-// O que fazer quando chegar notificação e o app estiver em segundo plano
+// O que fazer quando chegar notificação e o app estiver no bolso/segundo plano
 messaging.onBackgroundMessage(function(payload) {
     console.log('[firebase-messaging-sw.js] Notificação recebida em background.', payload);
     
